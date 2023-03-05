01490b029f4dffb299d903a3f23229ec0339a785
Radzim Kowalow <radzim_ko@wp.pl>
2023-03-05 16:35:48 +0100

    script config for tests, java apps switched to async

--- manifests/overlays/scenarios/1/script.js
+++ manifests/overlays/scenarios/1/script.js
@@ -2,15 +2,9 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '30s', target: 10 },
-    { duration: '1m', target: 10 },
-    { duration: '30s', target: 100 },
-    { duration: '1m', target: 100 },
-    { duration: '30s', target: 1000 },
-    { duration: '1m', target: 1000 },
-    { duration: '30s', target: 0 },
-  ],
+  executor: "constant-vus",
+  vus: 100,
+  duration: "5m"
 };
 
 export default function () {
